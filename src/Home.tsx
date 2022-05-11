e3a8be167c36a7e2541edb12c8b667c323c9ef38
tastycrackers <94281388+tastycrackers@users.noreply.github.com>
2022-05-11 14:32:24 +0800

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -31,29 +31,7 @@ const WalletContainer = styled.div`
 `;
 
 const WalletAmount = styled.div`
-  color: black;
-  width: auto;
-  padding: 5px 5px 5px 16px;
-  min-width: 48px;
-  min-height: auto;
-  border-radius: 5px;
-  
-  
-  box-sizing: border-box;
-  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-  font-weight: 500;
-  line-height: 1.75;
-  text-transform: uppercase;
-  border: 0;
-  margin: 0;
-  display: inline-flex;
-  outline: 0;
-  position: relative;
-  align-items: center;
-  user-select: none;
-  vertical-align: middle;
-  justify-content: flex-start;
-  gap: 10px;
+ 
 `;
 
 const Wallet = styled.ul`
