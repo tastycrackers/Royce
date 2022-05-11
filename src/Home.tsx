f2a5d1c77774ce8e99260dd2d5acb210b8800625
tastycrackers <94281388+tastycrackers@users.noreply.github.com>
2022-05-11 14:36:11 +0800

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -31,7 +31,29 @@ const WalletContainer = styled.div`
 `;
 
 const WalletAmount = styled.div`
- 
+  color: black;
+  width: auto;
+  padding: 5px 5px 5px 16px;
+  min-width: 48px;
+  min-height: auto;
+  border-radius: 5px;
+  background-color: var(--main-text-color);
+  box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);  
+  box-sizing: border-box;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  font-weight: 500;
+  line-height: 1.75;
+  text-transform: uppercase;
+  border: 0;
+  margin: 0;
+  display: inline-flex;
+  outline: 0;
+  position: relative;
+  align-items: center;
+  user-select: none;
+  vertical-align: middle;
+  justify-content: flex-start;
+  gap: 10px;
 `;
 
 const Wallet = styled.ul`
@@ -533,7 +555,7 @@ const Home = (props: HomeProps) => {
                 <br/>
                 <MintContainer>
                    
-                        <NFT elevation={3}>
+                       
                           
                             <br/>
                            <br/>
@@ -614,7 +636,7 @@ const Home = (props: HomeProps) => {
                             <br/>
                             {wallet && isActive && solanaExplorerLink &&
                               <SolExplorerLink href={solanaExplorerLink} target="_blank">View on Solscan</SolExplorerLink>}
-                        </NFT>
+                       
                     
                  
                 </MintContainer>
